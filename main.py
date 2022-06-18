4f7a36d2597d00390788d7a9fbd9ae1c265729a6
Saurin Anilkumar Prajapati <77115364+saurin1004@users.noreply.github.com>
2022-06-18 16:34:42 +0530

    Update main.py v2

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-application = Flask(__name__) #Initialize the flask App
+app = Flask(__name__) #Initialize the flask App
 model = pickle.load(open('model.pkl', 'rb'))
 
-@application.route('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@application.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
@@ -23,4 +23,4 @@ def predict():
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)
